2d3f91c39179457186eed430be61bea7fd353dc9
Kaylin Maharaj <Kaylin.Maharaj@sovtech.com>
2024-04-08 17:43:02 +0200

    GET current loadshedding stage

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, make_response
+import requests
+import os
+import json
+import urllib.parse
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    api_key = os.getenv('ESP_API_KEY')
+    request_headers = {'accept': 'application/json', 'Token': api_key}
+    req = request.get_json(silent=True, force=True)
+    intent = req["queryResult"]["intent"]["displayName"]
+    
+    answer = ""
+
+    if intent == 'LoadsSchedule':
+
+        status_url = "https://developer.sepush.co.za/business/2.0/status"
+        headers = {"Token": api_key}
+        
+        # Get status information
+        status_response = requests.get(status_url, headers=headers)
+        if status_response.status_code == 200:
+            status_data = status_response.json()
+            # Get Eskom stage
+            eskom_stage = int(status_data["status"]["eskom"]["stage"])
+            
+           
+
+        return make_response({
+            "fulfillmentText": answer,
+            "fulfillmentMessages": [
+                {
+                    "platform": "ACTIONS_ON_GOOGLE",
+                    "simpleResponses":{
+                        "simpleResponses": [
+                            {
+                                "textToSpeech": answer
+                            }
+                        ]
+                    }
+                }
+            ],
+            "source": "webhook"
+        })
+    
+
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
